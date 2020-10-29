b89c0044a4ebc8fda4b0e713ca1c0fe3f154ac58
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-30 01:50:53 +0500

    Update AddTransaction.tsx

--- src/components/AddTransaction.tsx
+++ src/components/AddTransaction.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddTransaction = () => {
+    
+    const [description, setDiscription] = useState();
+    const [transactionAmount, setTransactionAmount] = useState();
+
     return (
         <div>
             <h3> Add New Transaction </h3> 
+            <form> 
+                <div> 
+                    <label> Description of Transaction </label>
+                    <input
+                        type="text"
+                        id= "description"
+                        placeholder="Transaction details" 
+                        value={description}
+                    />
+                </div>
+                <div>
+                <label> Amount of Transaction </label>
+                    <input
+                        type="text"
+                        id= "descriptionAmount"
+                        placeholder="Enter Transaction Amount" 
+                        value={transactionAmount}
+
+                    />
+                </div>
+                <button> Add Transaction </button>
+            </form>
             
         </div>
     )
