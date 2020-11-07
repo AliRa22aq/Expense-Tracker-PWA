bcae54df371555553a99e5bdf2c423cd7191c8b0
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-11-07 16:19:23 +0500

    Update firebase.ts

--- src/components/firebase.ts
+++ src/components/firebase.ts
@@ -25,7 +25,6 @@ export function configNotification() {
 
         messaging.getToken().then((currentToken) => {
           if (currentToken) {
-                console.log("Token: ");
                 console.log(currentToken);
                 alert(currentToken)
           } else {
